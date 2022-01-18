eff2f80e21f7418c4631b5ad3525c63a8133c228
Eddie Ash <edwinashie289@outlook.com>
2022-01-18 15:18:50 +0000

    images cleared and settings seeder fixed

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -15,7 +15,7 @@ class SettingsTableSeeder extends Seeder
     public function run()
     {
         $setting = DB::table('settings')->insert([
-            'logo' => '/link',
+            'logo' => 'uploads/logo/1642518096foodies.png',
             'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem autem nam repellendus iure cupiditate, temporibus molestias nostrum consequuntur, numquam, soluta amet? Ex alias unde exercitationem, reiciendis ea maxime perferendis recusandae!'
         ]);
     }
